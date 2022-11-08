3f396e10dcd5c01b82541e38166e735be86f6eca
Gibran Pérez <eoncroz19@gmail.com>
2022-11-08 17:31:10 -0600

    Clase 7 usando matplotlib pie_charts

---
+++ charts/charts.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+def generate_pie_chart():
+  labels = ["A","B","C"]
+  values = [200,500,100]
+
+  fig, ax = plt.subplots()
+  ax.pie(values, labels=labels)
+  plt.savefig("pie.png")
+  plt.close()
+
+
+
+
+
+
+
+
+
+# concepto enviado en la cotizacion (supervisión de obra) clave corresponda al concepto.
+
+#servicios temporales de arquitectura
+
+
