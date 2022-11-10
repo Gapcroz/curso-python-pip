a5124985e81011f2b95ffecbdfeaba5a7e90b95e
Gibran Pérez <eoncroz19@gmail.com>
2022-11-10 12:52:38 -0600

    cambios

--- web-server/main.py
+++ web-server/main.py
@@ -5,16 +5,17 @@ app = fastAPI()
 
 @app.get('/')
 def get_list():
-  return [1,2,3]
+    return [1, 2, 3]
+
 
 @app.get('/contact')
 def get_list():
-  return {"name": 'platzi'}
+    return {"name": 'platzi'}
 
 
 def run():
-  store.get_categories()
+    store.get_categories()
 
 
 if __name__ == "__main__":
-  run()
+    run()
